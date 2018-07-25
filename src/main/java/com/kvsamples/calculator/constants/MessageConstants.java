ad0451fb2ad2b71bf11808cd95d6cc1505cfd985
vimal-kanagaraj <41547549+vimal-kanagaraj@users.noreply.github.com>
2018-07-25 13:53:49 +0530

    Changed Access modifiers

--- src/main/java/com/kvsamples/calculator/constants/MessageConstants.java
+++ src/main/java/com/kvsamples/calculator/constants/MessageConstants.java
@@ -11,8 +11,8 @@ public final class MessageConstants {
 		// do not instantiate this class
 	}
 
-	public static String NOT_A_VALID_EXPRESSION = "Not a valid expression";
-	public static String UNHANDLED_EXCEPTION = "Unable to evaluate the expression";
-	public static String INFINITE_VALUE = "Infinite Values";
+	public static final String NOT_A_VALID_EXPRESSION = "Not a valid expression";
+	public static final String UNHANDLED_EXCEPTION = "Unable to evaluate the expression";
+	public static final String INFINITE_VALUE = "Infinite Values";
 
 }
